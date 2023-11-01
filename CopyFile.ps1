38cb8eafff979dd05261fc5ad68221d6a0ee8dfa
Phillip Zeratul <p.f.zeratul@gmail.com>
2023-11-01 18:20:17 +0800

    Added shellscript for copy files
    Add a configuration than -destination_folder "D:\DOCUMENTS\Nutstore\DevVault\.obsidian\plugins\obsidian-para-with-tag"

---
+++ CopyFile.ps1
@@ -0,0 +1,10 @@
+param (
+[string]$destination_folder
+)
+
+if (!(Test-Path -path $destination_folder)) {
+    New-Item -ItemType Directory -Force -Path $destination_folder
+}
+Copy-Item "manifest.json" -Destination $destination_folder
+Copy-Item "main.js" -Destination $destination_folder
+Copy-Item "styles.css" -Destination $destination_folder
