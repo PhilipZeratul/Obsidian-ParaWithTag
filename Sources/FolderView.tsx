0492cf7c9a68c3e388c51a696acf79572824220d
Phillip Zeratul <p.f.zeratul@gmail.com>
2023-12-11 18:22:55 +0800

    Not sure what is being changed

--- Sources/FolderView.tsx
+++ Sources/FolderView.tsx
@@ -147,19 +147,21 @@ function NavFolder({folderData}: { folderData: NavTreeData }) {
 	
 	return (
 		<>
-			<div className={"tree-item nav-folder"}>
-				<div className={"tree-item-self is-clickable mod-collapsible nav-folder-title"} draggable={true}
-					 onClick={handleClick}>
-					<div className= {showChildren? "tree-item-icon collapse-icon nav-folder-collapse-indicator" : "tree-item-icon collapse-icon nav-folder-collapse-indicator is-collapsed"}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="svg-icon right-triangle">
-							<path d="M3 8L12 17L21 8"></path>
-						</svg>
+			<div style={{ animationDuration: "var(--anim-duration-fast)" }}>
+				<div className={showChildren? "tree-item nav-folder" : "tree-item nav-folder is-collapsed"}>
+					<div className={"tree-item-self is-clickable mod-collapsible nav-folder-title"} draggable={true}
+						 onClick={handleClick}>
+						<div className= {showChildren? "tree-item-icon collapse-icon nav-folder-collapse-indicator" : "tree-item-icon collapse-icon nav-folder-collapse-indicator is-collapsed"}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="svg-icon right-triangle">
+								<path d="M3 8L12 17L21 8"></path>
+							</svg>
+						</div>
+						<div className={"tree-item-inner nav-folder-title-content"}>
+							{folderData.name}
+						</div>
 					</div>
-					<div className={"tree-item-inner nav-folder-title-content"}>
-						{folderData.name}
-					</div>
+					{showChildren && <NavTree navTreeDatas={folderData.children}/>}
 				</div>
-				{showChildren && <NavTree navTreeDatas={folderData.children}/>}
 			</div>
 		</>
 	)
