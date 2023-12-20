0c52cd876bd5f35509d3ea0f142644e3f62466dc
Phillip Zeratul <p.f.zeratul@gmail.com>
2023-12-20 17:05:52 +0800

    Can animate when fold but still cant animate when unfold

--- Sources/FolderView.tsx
+++ Sources/FolderView.tsx
@@ -11,7 +11,7 @@ interface NavTreeData {
 }
 
 export function FolderView({app}: { app: App }) {
-	let folderTreeData: NavTreeData = {
+	let navTreeData: NavTreeData = {
 		id: "root",
 		name: app.vault.getName(),
 		isFile: false,
@@ -56,8 +56,7 @@ export function FolderView({app}: { app: App }) {
 	// TODO: Sort by name
 	for (let i = 0; i < files.length; i++) {
 		let paraProperty: string | null = null;
-		let currentData = folderTreeData.children[4];
-
+		let currentData = navTreeData.children[4]; // Default to Not PARA.
 		// TODO: Use app.FileManager.processfrontmatter()
 		let frontMatter = this.app.metadataCache.getFileCache(files[i])?.frontmatter;
 		if (frontMatter) {
@@ -66,10 +65,10 @@ export function FolderView({app}: { app: App }) {
 		if (paraProperty) {
 			// Add folder structure.
 			let folderStructure: string[] = paraProperty.split("/");
-			
+
 			switch (folderStructure[0]) {
 				case "Project":
-					currentData = folderTreeData.children[0];
+					currentData = navTreeData.children[0];
 					for (let j = 1; j < folderStructure.length; j++) {
 						let folderData = currentData.children.find(x => x.name == folderStructure[j]);
 						if (!folderData) {
@@ -95,7 +94,7 @@ export function FolderView({app}: { app: App }) {
 			}
 		}
 
-		let fileData: NavTreeData= {
+		let fileData: NavTreeData = {
 			id: currentData.children.length.toString(),
 			name: files[i].basename,
 			isFile: true,
@@ -104,17 +103,15 @@ export function FolderView({app}: { app: App }) {
 		currentData.children.push(fileData);
 	}
 
-	console.log(folderTreeData);
-	
 	return (
 		<>
 			<div className={"tree-item nav-folder mod-root"}>
 				<div className={"tree-item-self nav-folder-title"}>
 					<div className={"tree-item-inner nav-folder-title-content"}>
-						{folderTreeData.name}
+						{navTreeData.name}
 					</div>
 				</div>
-				<NavTree navTreeDatas={folderTreeData.children} isOpen={true}/>
+				<NavTree navTreeDatas={navTreeData.children}/>
 			</div>
 		</>
 	);
@@ -126,55 +123,54 @@ function usePrevious<T>(value: T) {
 	return ref.current
 }
 
-function NavTree({navTreeDatas, isOpen}: { navTreeDatas: NavTreeData[], isOpen: boolean }) {
+function NavTree({navTreeDatas}: { navTreeDatas: NavTreeData[] }) {
 	const folderDatas = navTreeDatas.filter(x => !x.isFile);
 	const fileDatas = navTreeDatas.filter(x => x.isFile);
 
-	// TODO: Add transition animation
-	const previous = usePrevious(isOpen)
-	const [ref, { height: viewHeight }] = useMeasure()
-	const { height, opacity, y } = useSpring({
-		from: { height: 0, opacity: 0, y: 0 },
-		to: {
-			height: isOpen ? viewHeight : 0,
-			opacity: isOpen ? 1 : 0,
-			y: isOpen ? 0 : 20,
-		},
-	})
-	
 	return (
 		<>
-			<animated.div className={"tree-item-children nav-folder-children"} style={{
-				opacity,
-				height: isOpen  ? 100 : 50,
-			}}>
-				<div style={{height: "0.1px", marginBottom: "0px"}} />
+			<div className={"tree-item-children nav-folder-children"}>
+				<div style={{height: "0.1px", marginBottom: "0px"}}/>
 				{folderDatas.map((node) => (
 					<NavFolder folderData={node} key={node.id}/>
 				))}
 				{fileDatas.map((node) => (
 					<NavFile fileData={node} key={node.id}/>
 				))}
-				<div ref={ref} />
-			</animated.div>
+			</div>
 		</>
 	);
 }
 
 function NavFolder({folderData}: { folderData: NavTreeData }) {
-	const [showChildren, setShowChildren] = useState(true);
+	const [isOpen, setIsOpen] = useState(true);
 
 	const handleClick = () => {
-		setShowChildren(!showChildren);
+		setIsOpen(!isOpen);
+		// setTimeout(() => {
+		// 	setUnmountChildren(!unmountChildren);
+		// }, 2000);
 	};
-	
+
+	// TODO: Add transition animation
+	const [ref, {height: viewHeight}] = useMeasure()
+	const {height} = useSpring({
+		from: {height: 0},
+		to: {
+			height: isOpen ? viewHeight : 0
+		},
+	})
+
 	return (
 		<>
-			<div className={showChildren? "tree-item nav-folder" : "tree-item nav-folder is-collapsed"}>
+			<div className={isOpen ? "tree-item nav-folder" : "tree-item nav-folder is-collapsed"}>
 				<div className={"tree-item-self is-clickable mod-collapsible nav-folder-title"} draggable={true}
 					 onClick={handleClick}>
-					<div className= {showChildren? "tree-item-icon collapse-icon nav-folder-collapse-indicator" : "tree-item-icon collapse-icon nav-folder-collapse-indicator is-collapsed"}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="svg-icon right-triangle">
+					<div
+						className={isOpen ? "tree-item-icon collapse-icon nav-folder-collapse-indicator" : "tree-item-icon collapse-icon nav-folder-collapse-indicator is-collapsed"}>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+							 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+							 className="svg-icon right-triangle">
 							<path d="M3 8L12 17L21 8"></path>
 						</svg>
 					</div>
@@ -182,7 +178,12 @@ function NavFolder({folderData}: { folderData: NavTreeData }) {
 						{folderData.name}
 					</div>
 				</div>
-				{showChildren && <NavTree navTreeDatas={folderData.children} isOpen={showChildren}/>}
+				<animated.div style={{
+					height: isOpen ? 'auto' : height, overflow: 'hidden',
+				}} ref={ref}>
+					{<NavTree navTreeDatas={folderData.children}/>}
+
+				</animated.div>
 			</div>
 		</>
 	)
