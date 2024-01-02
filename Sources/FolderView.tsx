d872d6a6d9667a2a36d1cfeba4ed0dda22959911
Phillip Zeratul <p.f.zeratul@gmail.com>
2024-01-02 16:03:39 +0800

    Have some progress, but dragged element rendered at the bottom and isDraggingOver style is propagated down

--- Sources/FolderView.tsx
+++ Sources/FolderView.tsx
@@ -1,9 +1,10 @@
 ﻿import {App, TFile} from "obsidian";
-import React, {useState} from "react";
+import React, {useState, useId} from "react";
 import {useSpring, animated} from "@react-spring/web";
 import useMeasure from 'react-use-measure'
-import { useRecoilState } from 'recoil';
-import * as RecoilState from 'Sources/Recoil/RecoilState'; 
+import {useRecoilState} from 'recoil';
+import * as RecoilState from 'Sources/Recoil/RecoilState';
+import {Draggable, Droppable} from '@hello-pangea/dnd';
 
 interface NavTreeData {
 	id: string;
@@ -13,7 +14,7 @@ interface NavTreeData {
 	file?: TFile;
 }
 
-export function FolderView({app}: { app: App }) {
+export function FolderView({app}: { app: App }) {	
 	let navTreeData: NavTreeData = {
 		id: "root",
 		name: app.vault.getName(),
@@ -129,20 +130,20 @@ function NavTree({navTreeDatas, app}: { navTreeDatas: NavTreeData[], app: App })
 		<>
 			<div className={"tree-item-children nav-folder-children"}>
 				<div style={{height: "0.1px", marginBottom: "0px"}}/>
-				{folderDatas.map((node) => (
-					<NavFolder folderData={node} key={node.id} app={app}/>
+				{folderDatas.map((node, index) => (
+					<NavFolder folderData={node} key={node.id} app={app} index={index}/>
 				))}
-				{fileDatas.map((node) => (
-					<NavFile fileData={node} key={node.id} app={app}/>
+				{fileDatas.map((node, index) => (
+					<NavFile fileData={node} key={node.id} app={app} index={index}/>
 				))}
 			</div>
 		</>
 	);
 }
 
-function NavFolder({folderData, app}: { folderData: NavTreeData, app:App }) {
+function NavFolder({folderData, app, index}: { folderData: NavTreeData, app: App, index: number }) {
 	const [isOpen, setIsOpen] = useState(true);
-	const [ref, {height: viewHeight}] = useMeasure()
+	const [measureRef, {height: viewHeight}] = useMeasure()
 	const {height} = useSpring({
 		from: {height: 0},
 		to: {
@@ -152,66 +153,77 @@ function NavFolder({folderData, app}: { folderData: NavTreeData, app:App }) {
 			duration: 100
 		}
 	})
-	
+
 	const handleClick = () => {
 		setIsOpen(!isOpen);
 	};
-	
+
 	return (
-		<>
-			<div className={"tree-item nav-folder" + (isOpen ? "" : " is-collapsed")}>
-				<div className={"tree-item-self is-clickable mod-collapsible nav-folder-title"} draggable={true}
-					 onClick={handleClick}>
-					<div
-						className={"tree-item-icon collapse-icon nav-folder-collapse-indicator" + (isOpen ? "" : " is-collapsed")}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-							 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-							 className="svg-icon right-triangle">
-							<path d="M3 8L12 17L21 8"></path>
-						</svg>
+		<Droppable droppableId={useId()}>
+			{(provided, snapshot) => (
+				<div className={"tree-item nav-folder" + (isOpen ? "" : " is-collapsed") + (snapshot.isDraggingOver ? " is-being-dragged-over" : "")}
+					 ref={provided.innerRef}
+					 {...provided.droppableProps}>
+					<div className={"tree-item-self is-clickable mod-collapsible nav-folder-title"}
+						 onClick={handleClick}>
+						<div
+							className={"tree-item-icon collapse-icon nav-folder-collapse-indicator" + (isOpen ? "" : " is-collapsed")}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+								 fill="none"
+								 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+								 className="svg-icon right-triangle">
+								<path d="M3 8L12 17L21 8"></path>
+							</svg>
+						</div>
+						<div className={"tree-item-inner nav-folder-title-content"}>
+							{folderData.name}
+						</div>
 					</div>
-					<div className={"tree-item-inner nav-folder-title-content"}>
-						{folderData.name}
-					</div>
+					<animated.div style={{height: height, overflow: 'hidden'}}>
+						<div ref={measureRef}>
+							{isOpen && <NavTree navTreeDatas={folderData.children} app={app}/>}
+						</div>
+					</animated.div>
+					{provided.placeholder}
 				</div>
-				<animated.div style={{
-					height: height, overflow: 'hidden',
-				}}>
-					<div ref={ref}>
-					{isOpen && <NavTree navTreeDatas={folderData.children} app={app}/>}
-					</div>
-				</animated.div>
-			</div>
-		</>
+			)}
+		</Droppable>
 	)
 }
 
-function NavFile({fileData, app}: { fileData: NavTreeData, app: App }) {
+function NavFile({fileData, app, index}: { fileData: NavTreeData, app: App, index: number }) {
 	const [activeFile, setActiveFile] = useRecoilState(RecoilState.activeFile);
 
 	let file = fileData.file as TFile;
-	
+
 	// TODO: Multi-select support. Shift click & Alt click.
 	const openFile = (e: React.MouseEvent) => {
 		if (file !== undefined) {
 			let newLeaf = (e.ctrlKey || e.metaKey) && !(e.shiftKey || e.altKey);
 			let leafBySplit = (e.ctrlKey || e.metaKey) && (e.shiftKey || e.altKey);
-			
+
 			let leaf = app.workspace.getLeaf(newLeaf);
 			if (leafBySplit) leaf = app.workspace.createLeafBySplit(leaf, "vertical");
 			app.workspace.setActiveLeaf(leaf);
 			leaf.openFile(file, {eState: {focus: true}}).then(r => setActiveFile(file));
 		}
 	};
-	
+
+	console.log("Draggable id = " + useId() + " index = " + index);
 	return (
-		<>
-			<div className={"tree-item nav-file"}>
-				<div className={"tree-item-self is-clickable nav-file-title" + (activeFile === file ? " is-active": "")} 
-					 draggable={true} onClick={(e) => openFile(e)}>
-					<div className={"tree-item-inner nav-file-title-content"}>{fileData.name}</div>
+		<Draggable draggableId={useId()} index={index}>
+			{(provided, snapshot) => (
+				<div className={"tree-item nav-file"}
+					 ref={provided.innerRef}
+					 {...provided.draggableProps}
+					 {...provided.dragHandleProps}>
+					<div
+						className={"tree-item-self is-clickable nav-file-title" + (activeFile === file ? " is-active" : "")}
+						onClick={(e) => openFile(e)}>
+						<div className={"tree-item-inner nav-file-title-content"}>{fileData.name}</div>
+					</div>
 				</div>
-			</div>
-		</>
+			)}
+		</Draggable>
 	)
 }
--- Sources/ParaWithPropertiesView.tsx
+++ Sources/ParaWithPropertiesView.tsx
@@ -3,6 +3,7 @@ import {createRoot, Root} from "react-dom/client";
 import {StrictMode} from "react";
 import {FolderView} from "./FolderView";
 import {RecoilRoot} from 'recoil';
+import {DragDropContext} from '@hello-pangea/dnd';
 
 export const PARA_WITH_TAGS_VIEW_TYPE = "para-with-tag-view";
 
@@ -26,13 +27,20 @@ export class ParaWithPropertiesView extends ItemView {
 	}
 
 	async onOpen() {
+		function onDragEnd() {
+			// the only one that is required
+			console.log("onDragEnd");
+		}
+		
 		this.root = createRoot(this.containerEl.children[1]);
 		this.root.render(
-			<RecoilRoot>
-				<StrictMode>
-					<FolderView app={this.app}/>
-				</StrictMode>
-			</RecoilRoot>
+			<StrictMode>
+				<RecoilRoot>
+					<DragDropContext onDragEnd={onDragEnd}>
+						<FolderView app={this.app}/>
+					</DragDropContext>
+				</RecoilRoot>
+			</StrictMode>
 		);
 	}
 
